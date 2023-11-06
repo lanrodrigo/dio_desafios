8ffd580a8422180378b9429eabcffc8973c5d4bc
lanrodrigo <ferreira.harlan@gmail.com>
2023-11-06 18:11:21 -0300

    Primeira versão inicial do Desafio do Herói

---
+++ desafio_do_heroi/codigo.js
@@ -0,0 +1,25 @@
+// Criação de variáveis necessárias
+let xp = 150000
+let nivel = ""
+let nome = "Hulk"
+
+// Criação de condições
+if(xp <= 1000){
+    nivel = "Ferro"
+} else if (xp > 1000 && xp <= 2000){
+    nivel = "Bronze"
+} else if (xp > 2000 && xp <= 5000){
+    nivel = "Prata"
+} else if (xp > 5000 && xp <= 7000){
+    nivel = "Ouro"
+} else if (xp > 7000 && xp <= 8000){
+    nivel = "Platina"
+} else if (xp > 8000 && xp <= 9000){
+    nivel = "Ascendente"
+} else if (xp > 9000 && xp <= 10000){
+    nivel = "Imortal"
+} else{
+    nivel = "Radiante"
+}
+
+console.log("O herói de nome " + nome + " está no nível " + nivel + " com " + xp + " pontos.")
